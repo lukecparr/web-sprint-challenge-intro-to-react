1c78bb56ffba6cbc13a03f2c7b44c9c105a803d0
Lukecp <lukecp@pm.me>
2020-11-29 23:08:18 -0500

    completed MVP

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,47 @@
 // Write your Character component here
+import React from 'react';
+import styled from 'styled-components';
+
+function Character(props) {
+
+	const { charData } = props;
+
+	const Container = styled.div`
+		padding: 1rem;
+		color: white;
+	`;
+
+	const Headers = styled.div`
+		display: flex;
+		justify-content: space-between;
+		padding: .2rem;
+		font-weight: bold;
+	`;
+
+	const Person = styled.div`
+		padding: .2rem;
+		text-align: left;
+		display: flex;
+		justify-content: space-between;
+	`;
+
+	const newCharData = Array.from(charData)
+
+	return (
+		<Container>
+			<Headers>
+				<div>Name</div>
+				<div>Eye Color</div>
+			</Headers>
+			
+			{newCharData.map(p => {
+				return <Person>
+							<div>{p.name}</div>
+							<div style={{color: p.eye_color}}>{p.eye_color}</div>
+						</Person>
+			})}
+		</Container>
+	);
+}
+
+export default Character;
